ea849b3bf15970cbe0b0fbd5fcb6c0ec7a09b47b
Muhammed Didin <mdidin80@gmail.com>
2022-08-19 19:18:06 +0300

    Initial

---
+++ Boost/Assets/Scripts/Movement.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] float mainThrust = 100f;
+    [SerializeField] float rotationThrust = 1f;
+    Rigidbody rb;
+    AudioSource audioSource;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        processThrust();
+        processRotation();
+    }
+
+    void processThrust() 
+    {
+        if(Input.GetKey(KeyCode.Space)) {
+            rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+            if(!audioSource.isPlaying){
+                audioSource.Play();
+            }
+        }
+        else{
+            audioSource.Stop();
+        }
+    }
+
+    void processRotation() {
+        if(Input.GetKey(KeyCode.A)) {
+            applyRotation(rotationThrust);
+
+        }
+        if(Input.GetKey(KeyCode.D)) {
+            applyRotation(-rotationThrust);
+        }
+    }
+
+    public void applyRotation(float rotationFrame) {
+        rb.freezeRotation = true; //freezing rotation so that we can manually rotate.
+        transform.Rotate(Vector3.forward * rotationFrame * Time.deltaTime);
+        rb.freezeRotation = false; //unfreezing rotation so physics system can take over.
+    }
+}
